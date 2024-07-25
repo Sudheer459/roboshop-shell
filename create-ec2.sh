40ee44b0df328b575c01abcb7df6dc8c6e2fc782
Sudheer459 <sudheer>
2024-07-25 14:31:25 +0530

    message

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
-domain_name="daws78s.online"
+
 
 for name in ${instances[@]}; do
     echo "Creating instance for: $name"
